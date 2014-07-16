66103de88411f62c524e1b8f3e47c635d606710c
Daniel van Dorp <daniel@vandorp.biz>
2014-07-16 18:01:49 +0200

    added input and output projections

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 __author__ = 'djvdorp'
 import shapefile
+import pyproj
 
 hectopunten_shp = shapefile.Reader("01-07-2014/Hectopunten/Hectopunten")
 hectopunten_shapes = hectopunten_shp.shapes()
@@ -8,3 +9,6 @@ print len(hectopunten_shapes)
 wegvakken_shp = shapefile.Reader("01-07-2014/Wegvakken/Wegvakken")
 wegvakken_shapes = wegvakken_shp.shapes()
 print len(wegvakken_shapes)
+
+input_projection = pyproj.Proj("+init=EPSG:28992") # Dit is Rijksdriehoekstelsel_New vanuit de .prj files, officieel EPSG:28992 Amersfoort / RD New
+output_projection = pyproj.Proj("+init=EPSG:4326") # LatLon with WGS84 datum used by GPS units and Google Earth, officieel EPSG:4326
