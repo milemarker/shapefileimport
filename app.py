cdd954144c51765ebef43279b302cf3d9f50338b
Daniel van Dorp <daniel@vandorp.biz>
2014-07-17 08:13:57 +0200

    basic output file writing implemented. need to verify the outputted now

--- app.py
+++ app.py
@@ -2,16 +2,33 @@ __author__ = 'djvdorp'
 import shapefile
 import pyproj
 
-hectopunten_input_reader = shapefile.Reader("01-07-2014/Hectopunten/Hectopunten")
+file_name = "01-07-2014/Hectopunten/Hectopunten"
+
+hectopunten_input_reader = shapefile.Reader(file_name)
 hectopunten_input_shapeRecords = hectopunten_input_reader.shapeRecords()
 #print len(hectopunten_shapes)
+
+# Show hectopunten fields to verify input
+hectopunten_fields = hectopunten_input_reader.fields
+print hectopunten_fields
+
+
 
 #wegvakken_shp = shapefile.Reader("01-07-2014/Wegvakken/Wegvakken")
 #wegvakken_shapes = wegvakken_shp.shapes()
 #print len(wegvakken_shapes)
 
+
+
 input_projection = pyproj.Proj("+init=EPSG:28992") # Dit is Rijksdriehoekstelsel_New vanuit de .prj files, officieel EPSG:28992 Amersfoort / RD New
 output_projection = pyproj.Proj("+init=EPSG:4326") # LatLon with WGS84 datum used by GPS units and Google Earth, officieel EPSG:4326
+
+# Create a shapefile writer using the same shape type as our reader
+hectopunten_output_writer = shapefile.Writer(hectopunten_input_reader.shapeType)
+# Copy over the existing dbf fields
+hectopunten_output_writer.fields = list(hectopunten_input_reader.fields)
+# Copy over the existing dbf records
+hectopunten_output_writer.records.extend(hectopunten_input_reader.records())
 
 index = 0
 while index < 100:
@@ -29,4 +46,10 @@ while index < 100:
         print "--- OUTPUT (WGS84): ---"
         print '({:-f}, {:-f})'.format(x, y)
         print "====================\n"
+
+        # Add the translated point to the new shapefile (output) to save it
+        hectopunten_output_writer.point(x, y)
     index += 1
+
+# Save output file to new shapefile
+hectopunten_output_writer.save()
