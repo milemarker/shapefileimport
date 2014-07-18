143a74c21a2305383b8ec782918680457ea99e0a
Daniel van Dorp <daniel@vandorp.biz>
2014-07-18 17:30:09 +0200

    functionized this shit

--- app.py
+++ app.py
@@ -2,54 +2,61 @@ __author__ = 'djvdorp'
 import shapefile
 import pyproj
 
-file_name = "01-07-2014/Hectopunten/Hectopunten"
 
-hectopunten_input_reader = shapefile.Reader(file_name)
-hectopunten_input_shapeRecords = hectopunten_input_reader.shapeRecords()
-#print len(hectopunten_shapes)
+def shp_transform_to_different_projection(file_name, src_projection, dest_projection, nr_of_shapes_to_process=None):
+    r = shapefile.Reader(file_name)
+    shapes = r.shapeRecords()
 
-# Show hectopunten fields to verify input
-hectopunten_fields = hectopunten_input_reader.fields
-print hectopunten_fields
+    nr_of_shapes_in_file = len(shapes)
+    if not nr_of_shapes_to_process:
+        nr_of_shapes_to_process = nr_of_shapes_in_file
+
+    print "{} shapes in file '{}'".format(nr_of_shapes_in_file, input_filename)
+
+    # Show fields to verify input
+    fields = r.fields
+    print fields
+
+    input_projection = pyproj.Proj(src_projection)
+    output_projection = pyproj.Proj(dest_projection)
+
+    # Create a shapefile writer using the same shape type as our reader
+    w = shapefile.Writer(r.shapeType)
+
+    # Copy over the existing dbf fields
+    w.fields = list(r.fields)
+
+    # Copy over the existing dbf records
+    w.records.extend(r.records())
+
+    index = 0
+    while index < nr_of_shapes_to_process:
+        for input_point in shapes[index].shape.points:
+            input_x = input_point[0]
+            input_y = input_point[1]
+
+            #print "===================="
+            #print "--- INPUT (Rijksdriehoekstelsel_New): ---"
+            #print '({:-f}, {:-f})'.format(input_x, input_y)
+
+            # Convert input_x, input_y from Rijksdriehoekstelsel_New to WGS84
+            x, y = pyproj.transform(input_projection, output_projection, input_x, input_y)
+
+            #print "--- OUTPUT (WGS84): ---"
+            #print '({:-f}, {:-f})'.format(x, y)
+            #print "====================\n"
+
+            # Add the translated point to the new shapefile (output) to save it
+            w.point(x, y)
+        index += 1
+
+    # Save output file to new shapefile
+    w.save("transformed")
 
 
+# Real action here
+input_filename = "01-07-2014/Hectopunten/Hectopunten"  # of "01-07-2014/Hectopunten/Hectopunten"
+input_projection_string = "+init=EPSG:28992"  # Dit is Rijksdriehoekstelsel_New vanuit de .prj files, officieel EPSG:28992 Amersfoort / RD New
+output_projection_string = "+init=EPSG:4326"  # LatLon with WGS84 datum used by GPS units and Google Earth, officieel EPSG:4326
 
-#wegvakken_shp = shapefile.Reader("01-07-2014/Wegvakken/Wegvakken")
-#wegvakken_shapes = wegvakken_shp.shapes()
-#print len(wegvakken_shapes)
-
-
-
-input_projection = pyproj.Proj("+init=EPSG:28992") # Dit is Rijksdriehoekstelsel_New vanuit de .prj files, officieel EPSG:28992 Amersfoort / RD New
-output_projection = pyproj.Proj("+init=EPSG:4326") # LatLon with WGS84 datum used by GPS units and Google Earth, officieel EPSG:4326
-
-# Create a shapefile writer using the same shape type as our reader
-hectopunten_output_writer = shapefile.Writer(hectopunten_input_reader.shapeType)
-# Copy over the existing dbf fields
-hectopunten_output_writer.fields = list(hectopunten_input_reader.fields)
-# Copy over the existing dbf records
-hectopunten_output_writer.records.extend(hectopunten_input_reader.records())
-
-index = 0
-while index < 100:
-    for input_point in hectopunten_input_shapeRecords[index].shape.points:
-        input_x = input_point[0]
-        input_y = input_point[1]
-
-        print "===================="
-        print "--- INPUT (Rijksdriehoekstelsel_New): ---"
-        print '({:-f}, {:-f})'.format(input_x, input_y)
-
-        # Convert input_x, input_y from Rijksdriehoekstelsel_New to WGS84
-        x, y = pyproj.transform(input_projection, output_projection, input_x, input_y)
-
-        print "--- OUTPUT (WGS84): ---"
-        print '({:-f}, {:-f})'.format(x, y)
-        print "====================\n"
-
-        # Add the translated point to the new shapefile (output) to save it
-        hectopunten_output_writer.point(x, y)
-    index += 1
-
-# Save output file to new shapefile
-hectopunten_output_writer.save()
+shp_transform_to_different_projection(input_filename, input_projection_string, output_projection_string, 1)
